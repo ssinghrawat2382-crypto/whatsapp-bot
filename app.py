51d66139a0fbfb4d857eec76ed70894552b236f3
Rawat SSR <ssinghrawat2382@gmail.com>
2026-04-16 05:41:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask
 
 app = Flask(__name__)
 
-# Home route (to check if server is running)
 @app.route("/")
 def home():
     return "Bot is running successfully"
 
-# WhatsApp webhook (main chatbot logic)
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    # Get message from WhatsApp user
-    incoming_msg = request.form.get("Body")
-
-    # Debug log (check in Render logs)
-    print("MESSAGE RECEIVED:", incoming_msg)
-
-    # Create Twilio response
-    response = MessagingResponse()
-    msg = response.message()
-
-    # Reply back to user
-    msg.body("You said: " + str(incoming_msg))
-
-    return str(response)
-
-# Run app (important for deployment)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
